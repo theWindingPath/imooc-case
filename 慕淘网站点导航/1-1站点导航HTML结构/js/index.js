3811b541f38debc1fd2c177ba382f092fdb75bc2
theWindingPath <2958202145@qq.com>
2018-10-22 15:37:30 +0800

    让菜单下拉，拼音重写

--- 慕淘网站点导航/1-1站点导航HTML结构/js/index.js
+++ 慕淘网站点导航/1-1站点导航HTML结构/js/index.js
@@ -1,5 +1,5 @@
-// // console.log(123);
-// // 第一种方法
+// // // console.log(123);
+// // // 第一种方法
 // $('.dropdown').hover(function(){ // 鼠标移入移出 hover接收两个参数
 //     var $dropdown = $(this);
 
@@ -18,7 +18,7 @@
 // }, function(){  //鼠标移出
 //     var $dropdown = $(this);
 
-//     $dropdown.find('.dropdown-toggle').css( // 鼠标移入 修改dropdown-toggle 的 css样式
+//     $dropdown.find('.dropdown-toggle').css( // 鼠标移出 修改dropdown-toggle 的 css样式
 //         {
 //             'background-color':'',  // 让它继承父类的背景颜色
 //             'border-color':''  // 没有分号
@@ -33,13 +33,13 @@
 
 // });
 
-// // 第二中方法
-// // 在 js中设置 css样式会产生 回流 和 重置
-// // console.log(123);
-// $('.dropdown').hover(function(){ // 鼠标移入移出 hover接收两个参数
-//     $(this).addClass('dropdown-active');
-// }, function(){  //鼠标移出
-//     $(this).removeClass('dropdown-active');
-// });
+// 第二中方法
+// 在 js中设置 css样式会产生 回流 和 重置
+// console.log(123);
+$('.dropdown').hover(function(){ // 鼠标移入移出 hover接收两个参数
+    $(this).addClass('dropdown-active');
+}, function(){  //鼠标移出
+    $(this).removeClass('dropdown-active');
+});
 
 // 第三种方法，不使用 js , 直接使用 css 中的 hover 实现
